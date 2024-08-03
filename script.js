0f8ab6479f3f9a31ca494829d4d12d5032e3cc1b
M Haidar Hanif <git@mhaidarhanif.com>
2024-08-03 13:43:19 +0700

    extract employee data from HTML to JS file

---
+++ script.js
@@ -0,0 +1,26 @@
+const dataEmployees = [
+  {
+    id: "1",
+    nama: "agas",
+    idPegawai: "109813",
+    golongan: "1",
+    departement: "Aplikasi Bisnis",
+    jabatan: "Officer",
+  },
+  {
+    id: "2",
+    nama: "agas1",
+    idPegawai: "109814",
+    golongan: "1",
+    departement: "Aplikasi Bisnis",
+    jabatan: "Officer",
+  },
+  {
+    id: "3",
+    nama: "agas",
+    idPegawai: "109815",
+    golongan: "1",
+    departement: "Aplikasi Bisnis",
+    jabatan: "Officer",
+  },
+];
